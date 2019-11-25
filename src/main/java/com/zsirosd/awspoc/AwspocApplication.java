0a752bfec9f6f47117d15cbddc9ea58d6c527c33
Dominik Zsiros <zsdominik@outlook.com>
2019-11-25 14:00:19 +0100

    init

---
+++ src/main/java/com/zsirosd/awspoc/AwspocApplication.java
@@ -0,0 +1,13 @@
+package com.zsirosd.awspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwspocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwspocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zsirosd/awspoc/AwspocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsirosd.awspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwspocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
